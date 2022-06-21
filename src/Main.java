ae7ec16df70eb236ba716bf963f3030c407d788d
hacker228973 <kodiy9@gmail.com>
2022-06-21 11:37:41 +0200

    1.1

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,14 @@ public class Main {
 
         for (int i = 0; i < 100; i++) {
             jobQueue.put(new NewThread());
+
+        }
+        for (int i = 0; i < 100; i++) {
+            NewThread thread = (NewThread) jobQueue.get();
+            thread.run();
+
         }
 
-        NewThread thread = (NewThread) jobQueue.get();
 
     }
 }
