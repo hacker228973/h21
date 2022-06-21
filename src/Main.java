2e9a792c227715f85815b965e08109d4b75db69f
hacker228973 <kodiy9@gmail.com>
2022-06-21 13:35:59 +0200

    1.6

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,58 @@
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         JobQueue jobQueue = new JobQueue();
+        Thread job =  new Thread(() -> {
+            while (true){
+                Runnable job1 = jobQueue.get();
+                job1.run();
+            }
+        });
+        job.start();
+for(int i =0;i<100;i++){
+    jobQueue.put(getJob());
+}
 
-        for (int i = 0; i < 100; i++) {
-            jobQueue.put(new NewThread());
 
+    }
+    public static Runnable getJob(){
+        return new Runnable() {
+            @Override
+            public void run() {
+
+
+                System.out.println("Job is started "+this);
+                try{
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Job have finished "+this);
+            }
+        };
+    }
+
+
+    static class JobQueue {
+        ArrayList<Runnable> jobs = new ArrayList<>();
+
+        public synchronized void put(Runnable job) {
+            jobs.add(job);
+            notify();
         }
-        for (int i = 0; i < 100; i++) {
-            NewThread thread = (NewThread) jobQueue.get();
-            thread.run();
 
+        public synchronized Runnable get()  {
+            while (jobs.isEmpty()) {
+                try {
+                    wait();
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+            Runnable job = jobs.get(0);
+            jobs.remove(job);
+            return job;
         }
-
-
     }
 }
